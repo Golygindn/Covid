056a4fe1e2963f83f841358be8773862dee82df1
Golygindn <39010151+Golygindn@users.noreply.github.com>
2020-04-07 19:26:37 +0300

    First commit

---
+++ Line-chart.js
@@ -0,0 +1,19 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['01.04.2020', '02.04.2020', '03.04.2020', '04.04.2020', '05.04.2020', '06.04.2020', '07.04.2020'],
+        datasets: [{
+            label: 'Заражённых',
+            /*backgroundColor: 'rgb(255, 99, 132)',*/
+            borderColor: 'rgb(255,64,84)',
+            data: [2563, 3548, 3834, 4355, 5389, 6343, 7497]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
